6a018dde2d19dc174f683cce96cb9ec438370d2f
william lee <will.lee.npc@gmail.com>
2023-12-22 21:53:44 -0500

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function onEvent(event) {
+            console.log("Good!")
+        }
+    function offEvent(event) {
+            console.log("Hey! Eyes on me!")
+    }
+      
+    
+
+    return <button onFocus={onEvent} onBlur={offEvent}>Eyes on me</button>
+}
+
+export default EyesOnMe
